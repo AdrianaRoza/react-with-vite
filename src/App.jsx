133f291d2881aff9b705b94e3bc49a0d249d1ea0
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-05-31 09:57:37 -0400

    feat: adding list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar"
 import Login from "./components/Login"
 import Button from "./components/Button"
+import List from "./components/List"
 
 const App = () => {
   return (
     <>
     <NavBar />
     <Login />
+    <List />
     <Button />
     </>
   )
