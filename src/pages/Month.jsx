8fc225c8f1918c5ede3bc99e92abce36acce453d
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-06-05 10:52:25 -0400

    feat: adding functionality in months.jsx

--- src/pages/Month.jsx
+++ src/pages/Month.jsx
@@ -1,16 +1,110 @@
+import React, { useState } from "react"
+
 const Month = () => {
-  const  months = ['Janeiro', 'Fevereiro', 'Março',
-    'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro',
-    'Novenbro', 'Dezembro']
+  const [monthsOfTheyear, setMonthsOfTheyear] = useState([
+    {
+      id:1,
+      week_pt:'Janeiro',
+      translation:'January',
+    },
+    {
+      id:2,
+      week_pt:'Fevereiro',
+      translation:'February',
+    },
+    {
+      id:3,
+      week_pt:'Março',
+      translation:'March',
+    },
+    {
+      id:4,
+      week_pt:'Abril',
+      translation:'April',
+    },
+    {
+      id:5,
+      week_pt:'Maio',
+      translation:'May',
+    },
+    {
+      id:6,
+      week_pt:'Junho',
+      translation:'June',
+    },
+    {
+      id:7,
+      week_pt:'Julho',
+      translation:'July',
+    },
+    {
+      id:8,
+      week_pt:'Agosto',
+      translation:'August',
+    },
+    {
+      id:9,
+      week_pt:'Setembro',
+      translation:'September',
+    },
+    {
+      id:10,
+      week_pt:'Outubro',
+      translation:'October',
+    },
+    {
+      id:11,
+      week_pt:'Novembro',
+      translation:'November',
+    },
+    {
+      id:12,
+      week_pt:'Dezembro',
+      translation:'December',
+    },
+  ])
+
+  const [editingTranslationId, setEditingTranslationId] = useState(null)
+  
+  const handleClick = (translationId) => {
+    setEditingTranslationId(translationId)
+  }
+
+  const handleChange = (event, translationId) => {
+    const updatedMonthsOfTheyear = monthsOfTheyear.map((translation) => {
+      if (translation.id === translationId) {
+        return { ...translation, week_pt: event.target.value }
+      }
+      return translation
+    })
+    setMonthsOfTheyear(updatedMonthsOfTheyear)
+  }
+
+  const getTranslatedWeek = (translationId) => {
+    const translation = monthsOfTheyear.find(
+      (translation) => translation.id === translationId
+    )
+    return translation ? translation.week_pt : ""
+  }
     
   return (
     <ul className="flex flex-col items-center mt-3 text-white">
-      {
-        months.map((month, i) => (
-          <li className="flex justify-center py-3 bg-rose-500 w-1/5 m-1
-              rounded-xl font-bold"  
+      {monthsOfTheyear.map((translation) => (
+        <li
+          key={translation.id}
+          className="flex justify-center py-3 bg-rose-500 w-1/5 m-1 rounded-xl font-bold"
+        >
+          <span
+            onClick={() => handleClick(translation.id)}
+            className={editingTranslationId === translation.id ? "hidden" : ""}
           >
-            { month }
+            {translation.translation}
+          </span>
+          <span
+            className={editingTranslationId === translation.id ? "" : "hidden"}
+          >
+            {getTranslatedWeek(translation.id)}
+          </span>
           </li>
         ))
       }
